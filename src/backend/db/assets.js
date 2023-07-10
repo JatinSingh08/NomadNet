99db0ec5e15e8efff95168b8d241b41d005c48fb
JatinSingh08 <singhjatin0812@gmail.com>
2023-07-10 15:11:30 +0530

    add: added more users

--- src/backend/db/assets.js
+++ src/backend/db/assets.js
@@ -5,6 +5,12 @@ export const swastikProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload
 export const devangProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688468827/devang-profile_jvxfur.jpg';
 export const yashProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688468827/yash-profile_spfv2n.jpg';
 export const johnProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688762755/JohnDoeProfile_w90pc6.jpg';
+export const juricaProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688980902/user1_mrcxvx.avif';
+export const josephProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688981045/joseph-gonzalez-iFgRcqHznqg-unsplash_qiemar.jpg';
+export const albertProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688981060/albert-dera-ILip77SbmOE-unsplash_ketv1s.jpg';
+export const slavProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688981078/slav-romanov-BrEAp01_m5w-unsplash_r4qnae.jpg';
+export const matheusProfile = 'https://res.cloudinary.com/ddldfhcup/image/upload/v1688980966/matheus-ferrero-W7b3eDUb_2I-unsplash_lnekhb.jpg';
+
 
 // Posts
 
